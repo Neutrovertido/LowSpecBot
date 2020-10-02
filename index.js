61302379fe79f5a44826eff45d29401c5b2711e7
Gasc1211 <gabriel.suazo1211@gmail.com>
2020-10-01 22:37:53 -0600

    Modify command handling

--- index.js
+++ index.js
@@ -1,63 +1,43 @@
-// Discord API
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const { prefix } = require("./config.json");
+const { error } = require("console");
+const fs = require("fs");
+
 const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
 
 // Environment Variables for Debugging
 try {
-    const dotenv = require('dotenv');
-    dotenv.config();
-} finally { }
+  const dotenv = require("dotenv");
+  dotenv.config();
+} finally {
+}
 
-// Prefixes
-const permittedPrefixes = ['~', '!', '$', '%', '^', '&', '*', '_'];
-let prefix = '_';
+// Commands Loading
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
-// Help Command Response
-const helpEmbed = new Discord.MessageEmbed()
-    .setColor('#010101')
-    .setTitle('Current Commands')
-    .setDescription(':one: **help:** Shows this message\n:two: **mc:** Displays the secret apple related message\n:three: **tehc-support:** Gives you tehc support\n:four: **bot:** Displays the **bot message**!\n:five: **prefix:** Changes the bot prefix.\n\nDefault prefix is **_**.')
-    .setTimestamp();
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  bot.commands.set(command.name, command);
+}
 
-// Welcome message for new members
-bot.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
-    if (!channel) return;
-    channel.send(`**Welcome to 🕹 LowSpecGuys 📺 , ${member}!**`);
-});
+// Commands Handling
+bot.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-// Bot commands
-bot.on('message', msg => {
-    if (!msg.guild) return;
-    switch (msg.content) {
-        case `${prefix}bot`:
-            msg.reply("**Windows XP Login Sounds**");
-            break;
-        case `${prefix}help`:
-            msg.channel.send(helpEmbed);
-            break;
-        case `${prefix}mc`:
-            msg.reply("**Manela Canzana**");
-            break;
-        case `${prefix}tehc-support`:
-            msg.reply("I'm going to eat you like **banana milkshake**");
-            break;
-    }
-});
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
 
-// Change Prefix
-bot.on('message', msg => {
-    if (!msg.guild) return;
-    if (msg.content === `${prefix}prefix`) {
-        msg.reply(`my prefix is ${prefix}.`);
-    } else if (msg.content.startsWith(`${prefix}prefix`)) {
-        if (permittedPrefixes.includes(msg.content[8])) {
-            prefix = msg.content[8];
-            msg.reply(`prefix changed to ${prefix}.`);
-        } else if (!permittedPrefixes.includes(msg.content[8])){
-            msg.reply(`prefix not permitted,  try another one.`);
-        }
-    }
+  if (!bot.commands.has(command)) return;
+
+  try {
+    bot.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("there was an error trying to execute that command!");
+  }
 });
 
 // Token Method
@@ -66,4 +46,6 @@ bot.login(token);
 console.log("Login successful!");
 
 // Execution Confirmation
-console.log("Bot initialized successfully!\nEnjoy the LSG Experience! 🎆")
+bot.once("ready", () => {
+  console.log("Bot initialized successfully!\nEnjoy the LSG Experience! 🎆");
+});
