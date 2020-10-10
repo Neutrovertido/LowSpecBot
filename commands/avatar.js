d6edcc837bcab85690f654ef210b40cba8e9f8e8
Neutrovertido <russellcode.james@gmail.com>
2020-10-09 18:35:28 -0600

    Improved avatar

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,13 +1,21 @@
+const { MessageEmbed } = require("discord.js")
+
 module.exports = {
   name: "avatar",
   description: "Display user avatar.",
   execute(message, args) {
     const user = message.author;
+    const avatarEmbed = new MessageEmbed()
+      .setColor("#FB3B5E")
+      .setTitle(`${user.username}'s avatar:`)
+      .setImage(
+        `${user.displayAvatarURL({
+          format: "png",
+          dynamic: true,
+        })}`
+      )
     return message.channel.send(
-      `${user.username}'s avatar: ${user.displayAvatarURL({
-        format: "png",
-        dynamic: true,
-      })}`
+      avatarEmbed
     );
   },
 };
