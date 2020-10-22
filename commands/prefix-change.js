8d0755d5368d06094d97b1d0a2c7e82f471cc7e7
Neutrovertido <russellcode.james@gmail.com>
2020-10-21 18:25:59 -0600

    Permissions HotFix

--- commands/prefix-change.js
+++ commands/prefix-change.js
@@ -6,16 +6,21 @@ module.exports = {
   name: "prefix",
   description: "Change commands prefix.",
   execute(message, args) {
-    if (permittedPrefixes.includes(args[0])) {
-      let config = JSON.parse(fs.readFileSync("./config.json").toString());
-      config.prefix = args[0];
-      fs.writeFileSync("./config.json", JSON.stringify(config));
-      message.reply(`prefix changed to ${args[0]}.`);
+    const author = message.guild.member(message.author);
+    if (author.hasPermission("ADMINISTRATOR")) {
+      if (permittedPrefixes.includes(args[0])) {
+        let config = JSON.parse(fs.readFileSync("./config.json").toString());
+        config.prefix = args[0];
+        fs.writeFileSync("./config.json", JSON.stringify(config));
+        message.reply(`prefix changed to ${args[0]}.`);
 
-    } else if (args.length === 0){
-      message.reply(`please specify a prefix.`)
+      } else if (args.length === 0) {
+        message.reply(`please specify a prefix.`)
+      } else {
+        message.reply(`prefix not permitted,  try another one.`);
+      }
     } else {
-      message.reply(`prefix not permitted,  try another one.`);
+      message.channel.send("You do not have permission to issue that command!");
     }
   },
 };
