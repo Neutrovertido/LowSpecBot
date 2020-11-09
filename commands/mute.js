e51d179055d9f3f7a3aed830e6c552337d462095
Neutrovertido <russellcode.james@gmail.com>
2020-11-08 21:02:07 -0600

    Various fixes

--- commands/mute.js
+++ commands/mute.js
@@ -3,14 +3,28 @@ module.exports = {
   description: "Mutes people in a voice channel.",
   execute(message, args) {
     const voiceChannel = message.member.voice.channel;
-    const members = voiceChannel.members;
-    members.forEach((member) => {
-      if (member.voice.mute === false) {
-        member.voice.setMute(true);
-        message.channel.send(
-          ":mute: **Successfully muted everyone in the voice channel!**"
-        );
+    const author = message.guild.member(message.author);
+    if (author.hasPermission("MUTE_MEMBERS")) {
+      try{
+        const members = voiceChannel.members;
+        let someone = false;
+        members.forEach((member) => {
+          if (member.voice.mute === false) {
+            someone = true;
+            member.voice.setMute(true);
+            message.channel.send(
+              ":mute: **Successfully muted everyone in the voice channel!**"
+            );
+          }
+          if (!someone) {
+            message.channel.send(`:warning: There's nobody to mute!`);
+          }
+        });
+      } catch {
+        message.channel.send(`:no_entry: You must be connected to a channel to issue this command!`);
       }
-    });
+    } else {
+      message.channel.send(`:no_entry: You don't have permission to perform that command!`)
+    }
   },
 };
--- commands/unmute.js
+++ commands/unmute.js
@@ -3,14 +3,28 @@ module.exports = {
   description: "Unmutes people in a voice channel.",
   execute(message, args) {
     const voiceChannel = message.member.voice.channel;
-    const members = voiceChannel.members;
-    members.forEach((member) => {
-      if (member.voice.mute === true) {
-        member.voice.setMute(false);
-        message.channel.send(
-          ":microphone2: **Successfully unmuted everyone in the voice channel!**"
-        );
+    const author = message.guild.member(message.author);
+    if (author.hasPermission("MUTE_MEMBERS")) {
+      try{
+        const members = voiceChannel.members;
+        let someone = false;
+        members.forEach((member) => {
+          if (member.voice.mute === true) {
+            someone = true;
+            member.voice.setMute(false);
+            message.channel.send(
+              ":microphone2: **Successfully unmuted everyone in the voice channel!**"
+            );
+          }
+          if (!someone) {
+            message.channel.send(`:warning: There's nobody to unmute!`);
+          }
+        });
+      } catch {
+        message.channel.send(`:no_entry: You must be connected to a channel to issue this command!`);
       }
-    });
+    } else {
+      message.channel.send(`:no_entry: You don't have permission to perform that command!`)
+    }
   },
 };
