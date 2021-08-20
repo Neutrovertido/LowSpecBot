c1ff1016e299e62a5836b63505c95be0cf5ea2a1
Gasc1211 <48170476+Gasc1211@users.noreply.github.com>
2021-08-20 12:35:04 -0600

    Fixed undefined glitch

--- commands/help.js
+++ commands/help.js
@@ -1,25 +1,27 @@
-const fs = require('fs');
+const fs = require("fs");
 const { MessageEmbed } = require("discord.js");
 
-const commands = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
-let description = "";
-
-for (let f of commands) {
-    let command = require(`./${f}`);
-    description += `\n**${command.name}**: ${command.description}`
-}
-
-const helpEmbed = new MessageEmbed()
-  .setColor("#010101")
-  .setTitle("Current Commands")
-  .setDescription(description)
-  .setTimestamp();
-
 module.exports = {
-    name: "help",
-    description: "Prints information about every command",
-    execute(message, args) {
-        message.channel.send(helpEmbed);
-        console.log("📜 Help message sent!");
+  name: "help",
+  description: "Prints information about every command",
+  execute(message, args) {
+    const commands = fs
+      .readdirSync("./commands")
+      .filter((file) => file.endsWith(".js"));
+    let description = "";
+
+    for (let f of commands) {
+      let command = require(`./${f}`);
+      description += `\n**${command.name}**: ${command.description}`;
     }
-}
+
+    const helpEmbed = new MessageEmbed()
+      .setColor("#010101")
+      .setTitle("Current Commands")
+      .setDescription(description)
+      .setTimestamp();
+
+    message.channel.send(helpEmbed);
+    console.log("📜 Help message sent!");
+  },
+};
